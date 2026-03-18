dd2103fc22e0fd5c19bc92fec7d7a87982a2fd5b
DefDumbCode <dimas07072007@gmail.com>
2026-03-18 22:48:26 +0300

    Создание проекта для тестирования

---
+++ BankAccount.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        /// <summary>
+        /// Приватный конструктор класса
+        /// </summary>
+        private BankAccount() { }
+
+        /// <summary>
+        /// Публичный конструктор банковского счета
+        /// </summary>
+        /// <param name="customerName"></param>
+        /// <param name="balance"></param>
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        /// <summary>
+        /// Получает имя пользователя
+        /// </summary>
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        /// <summary>
+        /// Получает текущий баланс пользователя
+        /// </summary>
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        /// <summary>
+        /// Списывает указанную сумму со счета
+        /// </summary>
+        /// <param name="amount">Сумма списания</param>
+        /// <exception cref="ArgumentOutOfRangeException">Сумма снятия отрицательная или превышает баланс</exception>
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        /// <summary>
+        /// Зачисляет указанную сумму на счет
+        /// </summary>
+        /// <param name="amount">Сумма зачисления</param>
+        /// <exception cref="ArgumentOutOfRangeException">Сумма зачисления отрицательна</exception>
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+            Console.ReadLine();
+        }
+    }
+}
